7fefd1249bdf23858f89f9b76bf077cfa158dbd6
vinitus <tlsdnrng@gmail.com>
2022-11-16 19:16:16 +0900

    custom user model

--- whattowatch/accounts/models.py
+++ whattowatch/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.TextField()
+    gender = models.TextField()
+    birthday = models.DateField()
+    phone_number = models.IntegerField()
--- whattowatch/feed/models.py
+++ whattowatch/feed/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    content =  models.TextField()
+    score = models.FloatField()
+    watched = models.BooleanField()
+    
