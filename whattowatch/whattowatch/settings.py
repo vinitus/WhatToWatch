67963893fa8549ec6db5d2f96a824ea3bc45c58b
vinitus <tlsdnrng@gmail.com>
2022-11-17 11:33:54 +0900

    conflict resovlve, cors header

--- whattowatch/whattowatch/settings.py
+++ whattowatch/whattowatch/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'api',
     'accounts',
     'feed',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
@@ -56,12 +57,15 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     # Authentication
